c38cab5d496706e6fce232d23c5dee62df76f11e
jathin12e <126076303+jathin12e@users.noreply.github.com>
2025-06-11 19:11:30 +0530

    commit

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -23,7 +23,7 @@ const project = () => {
           <img src="https://res.cloudinary.com/dgzuiurhr/image/upload/v1746204015/Screenshot_2025-05-02_220839_eso5ot.png" className='image2' />
           <h3>Extract Text from PDF and Image</h3>
           <div className='overlay'>
-            <a href='https://774a-2409-40f0-11cf-8b68-e863-e2f3-acc3-1ea7.ngrok-free.app/' target='_blanck'><button className='button1 text'>Live Demo</button></a>
+            <a href='https://9ec9-2405-201-c02a-f107-35b9-2ea4-fc58-8dd6.ngrok-free.app//' target='_blanck'><button className='button1 text'>Live Demo</button></a>
           </div>
           <p className='description'>AI app using GPT-4 and OCR to extract and query text from PDFs and images.<br />Technologies: Python, OpenCV, PyMuPDF, Tesseract</p>
         </div>
